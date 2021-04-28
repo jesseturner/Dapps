0ffed0554a7a406e928e3cb4fa24a71b11296753
Jesse Turner <jesseturner@Jesses-MacBook-Pro.local>
2021-04-27 22:57:33 -0600

    Part 3 of tutorial done.

--- src/app.js
+++ src/app.js
@@ -104,7 +104,13 @@ App = {
   	// Show the task
   	$newTaskTemplate.show()
   }
-  
+  },
+
+  createTask: async () => {
+  	App.setLoading(true)
+  	const content = $('#newTask').val()
+  	await App.todoList.createTask(content)
+  	window.location.reload()
   },
 
   setLoading: (boolean) => {
